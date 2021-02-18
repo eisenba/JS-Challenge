1146eaed200193cc1f9883feb901a11a8cfd7e84
Matt Eisenbart <matt.eisenbart@gmail.com>
2021-02-18 12:03:36 -0600

    Started click event

--- static/js/app.js
+++ static/js/app.js
@@ -3,6 +3,26 @@ var tableData = data;
 console.log("data :: ", data);
 // YOUR CODE HERE!
 const tableBody = d3.select("#ufo-table tbody");
+function runEnter() {
+  // select the table
+  var list = d3.select(".tbody");
+
+  // remove any children from the list to
+  list.html("")
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  console.log(people);
+
+  var filteredData = tableData.filter((sighting) => sighting.bloodType === inputValue);
 
 // tableBody.html = "";
 let tableRow = tableBody.append("tr");
